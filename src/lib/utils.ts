f1277645321c3ec248de503572812c30f02346ec
Samuelxiao <335843707@qq.com>
2024-12-14 12:17:09 +0800

    Your commit message here

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-export const tryParseJson = (x: any) => {
-  try {
-    return JSON.parse(x);
-  } catch (e) {
-    return undefined;
-  }
-};
-
-export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
